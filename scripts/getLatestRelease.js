43a30ec24cb235f344cede97936e8332e452897a
Rodolfo Pereira <phinpho@gmail.com>
2024-08-02 12:41:45 +0200

    fix: include the custom script to be executed in the workflow

---
+++ scripts/getLatestRelease.js
@@ -0,0 +1,11 @@
+module.exports = ({ github, context, core }) => {
+  const { data: { draft, prerelease, name, assets } } = await github.rest.repos.getLatestRelease({
+    owner: context.repo.owner,
+    repo: "sceneforge",
+  });
+  core.exportVariable('draft', draft);
+  core.exportVariable('prerelease', prerelease);
+  core.exportVariable('name', name);
+  const artifact = assets.find(({ name }) => name === 'sceneforge.tar.gz');
+  core.exportVariable('artifact', artifact?.browser_download_url);
+};
