5fb90d6c466386e42a9cb8504d5992e0237b8dc7
Rodolfo Pereira <phinpho@gmail.com>
2024-08-03 12:18:25 +0200

    fix: add download script to the workflow

--- scripts/getLatestRelease.js
+++ scripts/getLatestRelease.js
@@ -1,11 +1,29 @@
-module.exports = async ({ github, context, core }) => {
+import download from "./download.js";
+
+const package = 'sceneforge.tar.gz';
+
+export default async ({ github, context, core }) => {
   const { data: { draft, prerelease, name, assets } } = await github.rest.repos.getLatestRelease({
     owner: context.repo.owner,
     repo: "sceneforge",
   });
+
   core.exportVariable('draft', draft);
   core.exportVariable('prerelease', prerelease);
+
   core.exportVariable('name', name);
-  const artifact = assets.find(({ name }) => name === 'sceneforge.tar.gz');
-  core.exportVariable('artifact', artifact?.browser_download_url);
+
+  const artifact = assets.find(({ name }) => name === package)?.browser_download_url;
+
+  core.exportVariable('artifact', artifact);
+
+  if (artifact) {
+    const result = await download(artifact?.browser_download_url, package);
+  }
+
+  console.log("DEBUG: draft", draft);
+  console.log("DEBUG: prerelease", prerelease);
+  console.log("DEBUG: name", name);
+  console.log("DEBUG: artifact", artifact);
+  console.log("DEBUG: result", result);
 };
