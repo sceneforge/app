203148251d2f51228f0c290b507d806a5ba0b44a
Rodolfo Pereira <phinpho@gmail.com>
2024-08-03 13:32:39 +0200

    chore: remove debug logs

--- scripts/getLatestRelease.js
+++ scripts/getLatestRelease.js
@@ -10,32 +10,19 @@ module.exports = async ({ github, context, core, workspace }) => {
   });
 
   core.exportVariable('RELEASE_DRAFT', draft);
-  console.log("DEBUG: RELEASE_DRAFT", draft);
-
   core.exportVariable('RELEASE_PRERELEASE', prerelease);
-  console.log("DEBUG: RELEASE_PRERELEASE", prerelease);
-
   core.exportVariable('RELEASE_NAME', name);
-  console.log("DEBUG: RELEASE_NAME", name);
 
   const artifact = assets.find(({ name }) => name === package)?.browser_download_url;
 
   core.exportVariable('RELEASE_ARTIFACT', artifact);
-  console.log("DEBUG: RELEASE_ARTIFACT", artifact);
 
   if (artifact) {
     const result = await download(artifact, join(workspace, package));
 
     core.exportVariable('RELEASE_ARTIFACT_PATH', result.path);
-    console.log("DEBUG: RELEASE_ARTIFACT_PATH", result.path);
-
     core.exportVariable('RELEASE_ARTIFACT_SIZE', result.size);
-    console.log("DEBUG: RELEASE_ARTIFACT_SIZE", result.size);
-
     core.exportVariable('RELEASE_ARTIFACT_BASENAME', result.basename);
-    console.log("DEBUG: RELEASE_ARTIFACT_BASENAME", result.basename);
-
     core.exportVariable('RELEASE_ARTIFACT_FILENAME', result.filename);
-    console.log("DEBUG: RELEASE_ARTIFACT_FILENAME", result.filename);
   }
 };
